c16f3db8885f9a1cfe6c9b3018654728dbbb0d1a
chanwooLim <hvcloc@gmail.com>
2024-02-08 00:44:31 +0900

    # User APP - Custom Model

--- houses/admin.py
+++ houses/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
+
 from .models import House
 
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-
-    list_display = [
-        "name",
-        "price_per_night",
-        "address",
-        "pets_allowed",
-    ]
-    list_filter = [
-        "price_per_night",
-        "pets_allowed",
-    ]
-    search_fields = ["address__startswith"]
+    list_display = ("name", "price_per_night", "address", "pets_allowed")
+    list_filter = ("price_per_night", "pets_allowed")
