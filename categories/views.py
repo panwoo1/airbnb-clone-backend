155884f4babb08beae4b591519cc77d7687b8785
chanwooLim <hvcloc@gmail.com>
2024-02-20 17:33:21 +0900

    # DRF - JsonResponse

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Category
+
+
+def categories(request):
+    all_cateogries = Category.objects.all()
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": all_cateogries,
+        }
+    )
