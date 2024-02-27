8b43ea0e4c394ac0a6203343ae7fbc752e85efd8
chanwooLim <hvcloc@gmail.com>
2024-02-27 21:52:18 +0900

    # REST - is_lliked

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,6 +4,7 @@ from categories.serializers import CategorySerializer
 from medias.serializers import PhotoSerializer
 from reviews.serializers import ReviewSerializer
 from users.serializers import TinyUserSerializer
+from wishlists.models import Wishlist
 
 from .models import Amenity, Room
 
@@ -29,6 +30,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     )
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -41,6 +43,10 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(user=request.user, rooms__pk=room.pk).exists()
 
 
 class RoomListSerializer(serializers.ModelSerializer):
