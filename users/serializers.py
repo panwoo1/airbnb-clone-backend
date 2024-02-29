7a238973c553428606e924e483673503e6f4273e
chanwooLim <hvcloc@gmail.com>
2024-02-29 19:18:48 +0900

    # Users API

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import User
 
 
-class TinyUserSerializer(ModelSerializer):
+class TinyUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
@@ -12,3 +12,44 @@ class TinyUserSerializer(ModelSerializer):
             "avatar",
             "username",
         )
+
+
+class PrivateUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "id",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
+        )
+
+
+class PublicSerializer(serializers.ModelSerializer):
+
+    rooms_count = serializers.SerializerMethodField()
+    reviews_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "avatar",
+            "name",
+            "gender",
+            "rooms_count",
+            "reviews_count",
+        )
+
+    def get_rooms_count(self, user):
+        return user.rooms.count()
+
+    def get_reviews_count(self, user):
+        return user.reviews.count()
