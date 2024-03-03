728fd01a8ba10ef37836268f1998f2dbe770722b
chanwooLim <hvcloc@gmail.com>
2024-03-04 08:23:44 +0900

    # Graphql - Refactor

--- config/schema.py
+++ config/schema.py
@@ -16,26 +16,29 @@ movies_db = [
 ]
 
 
+def movies() -> typing.List[Movie]:
+    return movies_db
+
+
+def movie(movie_pk: int) -> Movie:
+    return movies_db[movie_pk - 1]
+
+
 @strawberry.type
 class Query:
+    movies: typing.List[Movie] = strawberry.field(resolver=movies)
+    movie: Movie = strawberry.field(resolver=movie)
 
-    @strawberry.field
-    def movies(self) -> typing.List[Movie]:
-        return movies_db
 
-    @strawberry.field
-    def movie(self, movie_pk: int) -> Movie:
-        return movies_db[movie_pk - 1]
+def add_movie(title: str, year: int, rating: int) -> Movie:
+    new_movie = Movie(pk=len(movies_db) + 1, title=title, year=year, rating=rating)
+    movies_db.append(new_movie)
+    return new_movie
 
 
 @strawberry.type
 class Mutation:
-
-    @strawberry.mutation
-    def add_movie(self, title: str, year: int, rating: int) -> Movie:
-        new_movie = Movie(pk=len(movies_db) + 1, title=title, year=year, rating=rating)
-        movies_db.append(new_movie)
-        return new_movie
+    add_movie: Movie = strawberry.mutation(resolver=add_movie)
 
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
