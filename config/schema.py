c33a664c568af208b368f00f166002de7984d727
chanwooLim <hvcloc@gmail.com>
2024-03-03 22:23:53 +0900

    # Graphql - Query Arguments

--- config/schema.py
+++ config/schema.py
@@ -1,12 +1,31 @@
+import typing
+
 import strawberry
+
+
+@strawberry.type
+class Movie:
+    pk: int
+    title: str
+    year: int
+    rating: int
+
+
+movies_db = [
+    Movie(pk=1, title="Godfater", year=1990, rating=10),
+]
 
 
 @strawberry.type
 class Query:
 
     @strawberry.field
-    def ping(self) -> str:
-        return "pong"
+    def movies(self) -> typing.List[Movie]:
+        return movies_db
+
+    @strawberry.field
+    def movie(self, movie_pk: int) -> Movie:
+        return movies_db[movie_pk - 1]
 
 
 schema = strawberry.Schema(query=Query)
