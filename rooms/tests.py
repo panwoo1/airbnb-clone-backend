3ab1b069581940add33cd62511fb1a12fa5b5af9
chanwooLim <hvcloc@gmail.com>
2024-03-08 10:16:03 +0900

    # API Testing - Amenity Detail Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -36,6 +36,8 @@ class TestAmenities(APITestCase):
 
         new_amenity_name = "New Amenity"
         new_amenity_desc = "New Amenity desc."
+        name_more_than_150 = "a" * 500
+
         response = self.client.post(
             self.URL,
             data={"name": new_amenity_name, "description": new_amenity_desc},
@@ -52,4 +54,61 @@ class TestAmenities(APITestCase):
         response = self.client.post(self.URL)
 
         self.assertEqual(response.status_code, 400)
-        print(response.json())
+
+        response = self.client.post(
+            self.URL, data={"name": name_more_than_150, "description": new_amenity_desc}
+        )
+        data = response.json()
+        self.assertIn("name", data)
+
+
+class TestAmenity(APITestCase):
+    NAME = "Test Amenity"
+    DESC = "Test DESC"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+
+        response = self.client.get("/api/v1/rooms/amenities/1")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(data["name"], self.NAME)
+        self.assertEqual(data["description"], self.DESC)
+
+    def test_put_amenity(self):
+
+        updated_name = "Update name"
+        updated_desc = "Update desc"
+        name_more_than_150 = "a" * 300
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={"name": updated_name, "description": updated_desc},
+        )
+        data = response.json()
+        self.assertEqual(data["name"], updated_name)
+        self.assertEqual(data["description"], updated_desc)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={"name": name_more_than_150, "desc": updated_desc},
+        )
+        data = response.json()
+        self.assertEqual("name", data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_amenity(self):
+        response = self.client.delete("/api/v1/rooms/amenities/1")
+        self.assertEqual(response.status_code, 204)
